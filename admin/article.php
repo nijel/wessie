619185bc298f87a1139553a26ae72213daa84757
Michal Čihař <michal@cihar.com>
2001-07-22 18:46:48 +0000

    article administration

--- admin/article.php
+++ admin/article.php
@@ -27,5 +27,53 @@
 
 $page_name='Articles';
 require_once('./admin_header.php');
+?>
+<form method="GET">
+Language: <select name="filter_lng">
+  <option value="any">Any</option>
+<?php
+for($i=0;$i<count($languages);$i++){
+
+echo '<option value='.$i.((isset($filter_lng) && ((int)$filter_lng)==$i)?' selected':'').'>'.$lang_name[$i]."</option>\n";
+}
+?>
+</select>
+<input type="submit">
+</form><br>
+
+
+<?php
+
+if (isset($filter_lng) && $filter_lng!='any') {
+    $cond = ' and '.$table_prepend_name.$table_page.'.lng='.$filter_lng;
+} else {
+    $cond = '';
+}
+
+if (!$id_result=mysql_query(
+'SELECT content, last_change, page, '.$table_prepend_name.$table_article.'.lng as lng, id, name, description, count, category'.
+' from '.$table_prepend_name.$table_article.','.$table_prepend_name.$table_page.
+' where id=page and '.$table_prepend_name.$table_article.'.lng='.$table_prepend_name.$table_page.'.lng'.$cond.
+' order by page,lng'))
+    do_error(1,'SELECT '.$table_prepend_name.$table_article.','.$table_prepend_name.$table_page.': '.mysql_error());
+
+if (mysql_num_rows($id_result) == 0){
+echo "Nothing...";
+} else {
+    echo 'Displayed articles: '.mysql_num_rows($id_result);
+    echo "<table border=0><tr><th>Page</th><th>Title</th><th>Description</th><th>Language</th><th colspan=2>Action</th></tr>\n";
+    $even=1;
+    while ($item = mysql_fetch_array ($id_result)) {
+        if ($even == 1) {
+            echo '<tr bgcolor="#5f5f5f"><td>';
+        } else {
+            echo '<tr bgcolor="#8f8f8f"><td>';
+        }
+        $even = 1 - $even;
+        echo $item['page'].'</td><td>'.$item['name'].'</td><td>'.$item['description'].'</td><td>'.$lang_name[$item['lng']].'</td><td><a href="article_edit.php?id=',$item['page'].'&lng='.$item['lng'].'">Edit</a></td><td><a href="article_delete.php?id=',$item['page'].'&lng='.$item['lng'].'">Delete</a></td></tr>'."\n";
+    }
+    echo "</table>\n";
+}
+
 require_once('./admin_footer.php');
 ?>
