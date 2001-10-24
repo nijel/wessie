e6b244c4870b109f28e33afc085302bb2dd3a44c
Michal Čihař <michal@cihar.com>
2001-10-24 18:57:13 +0000

    menu editing works...

--- admin/menu.php
+++ admin/menu.php
@@ -38,57 +38,55 @@ category_edit(isset($category)?$category:-1,isset($lng)?$lng:0,'category');
 
 function add_childs($child_id,$depth){
 global $site_name,$site_author,$site_author_email,$site_name,$site_home,$page_title,$category_name,$wessie_version,$wessie_author,$browser,$os,
-        $wessie_author_email,$wessie_url,$SERVER_SOFTWARE,$SERVER_SIGNATURE,$SERVER_PROTOCOL,$SERVER_NAME,$SERVER_ADDR,$SERVER_PORT,$HTTP_USER_AGENT,
-        $REQUEST_URI,$REMOTE_ADDR,$HTTP_REFERER, $base_path;
+    $wessie_author_email,$wessie_url,$SERVER_SOFTWARE,$SERVER_SIGNATURE,$SERVER_PROTOCOL,$SERVER_NAME,$SERVER_ADDR,$SERVER_PORT,$HTTP_USER_AGENT,
+    $REQUEST_URI,$REMOTE_ADDR,$HTTP_REFERER, $base_path;
 global $db_connection,$table_menu,$table_page,$table_prepend_name,$lng,$category,$even;
 
 if (!($id_result=(mysql_query('SELECT id,name,description,page,category,parent,expand,rank from '.$table_prepend_name.$table_menu.' where lng='.$lng.' and parent='.$child_id.' and category='.$category.' order by rank',$db_connection)))&&($child_id=0))
-        do_error(1,'SELECT '.$table_prepend_name.$table_menu.': '.mysql_error());
+    do_error(1,'SELECT '.$table_prepend_name.$table_menu.': '.mysql_error());
+
 while ($item = mysql_fetch_array ($id_result)){
+    if (!($id2_result=mysql_query('SELECT name,description from '.$table_prepend_name.$table_page.' where lng='.$lng.' and id='.$item['page'].' limit 1',$db_connection)))
+        do_error(1,'SELECT '.$table_prepend_name.$table_page.': '.mysql_error());
+    $page=mysql_fetch_array($id2_result);
+    mysql_free_result($id2_result);
 
-        if (!($id2_result=mysql_query('SELECT name,description from '.$table_prepend_name.$table_page.' where lng='.$lng.' and id='.$item['page'].' limit 1',$db_connection)))
-                do_error(1,'SELECT '.$table_prepend_name.$table_page.': '.mysql_error());
-        $page=mysql_fetch_array($id2_result);
-        mysql_free_result($id2_result);
+    if ((!isset($item['name']))||($item['name']=='')) $name=$page['name'];
+    else $name=$item['name'];
 
-        if ((!isset($item['name']))||($item['name']=='')) $name=$page['name'];
-        else $name=$item['name'];
-
-        if ((!isset($item['description']))||($item['description']=='')) $desc=$page['description'];
-        else $desc=$item['description'];
-
+    if ((!isset($item['description']))||($item['description']=='')) $desc=$page['description'];
+    else $desc=$item['description'];
 
 
-        if ($even == 1) {
-            echo '<tr bgcolor="#5f5f5f"><td>';
-        } else {
-            echo '<tr bgcolor="#8f8f8f"><td>';
-        }
-        $even = 1 - $even;
 
-        for ($i=0; $i<$depth; $i++) echo '&nbsp;&nbsp;';
+    if ($even == 1) {
+        echo '<tr bgcolor="#5f5f5f"><td>';
+    } else {
+        echo '<tr bgcolor="#8f8f8f"><td>';
+    }
+    $even = 1 - $even;
 
-        eval('?' . '>' . $name . '</td><td>' . $desc );
-        echo '</td><td>' . $depth;
-        echo '</td><td>' . $item['rank'];
-        echo '</td><td>' . $item['expand'];
-        echo '</td><td><a href="menu_edit.php?id=',$item['page'].'&amp;lng='.$lng.'">Edit</a></td><td><a href="menu_edit.php?parent=',$item['page'].'&amp;lng='.$lng.'">Add child</a></td><td><a href="menu_delete.php?id=',$item['page'].'&amp;lng='.$lng.'">Delete</a></td><td><a href="'.make_url($item['page'],$lng).'" target="_blank">View</a></td></tr>'."\n";
+    for ($i=0; $i<$depth; $i++) echo '&nbsp;&nbsp;';
 
-//        eval('?'.'>'.make_menu_item(make_url($item['page'],$lng),$name,$category['name'],$category['short'],$desc,$item['page']==$id,$depth).'<?php ');
-//        if (($item['expand']==1) || ($item['page']==$id) || in_array ($item['id'],$parents)){
-            add_childs($item['id'],$depth+1);
-        //}
+    echo htmlspecialchars($name) . '</td><td>' . htmlspecialchars($desc) ;
+    echo '</td><td>' . $depth;
+    echo '</td><td>' . $item['rank'];
+    echo '</td><td>' . $item['expand'];
+    echo '</td><td><a href="menu_edit.php?id=',$item['page'].'&amp;lng='.$lng.'">Edit</a></td><td><a href="menu_edit.php?parent=',$item['page'].'&amp;lng='.$lng.'">Add child</a></td><td><a href="menu_delete.php?id=',$item['page'].'&amp;lng='.$lng.'">Delete</a></td><td><a href="'.make_url($item['page'],$lng).'" target="_blank">View</a></td></tr>'."\n";
+
+    add_childs($item['id'],$depth+1);
 }
 mysql_free_result($id_result);
 }
 
 
 if (isset($category)){
+    echo '<em>' .$lang_name[$lng].':'.htmlspecialchars(get_category_name($category,$lng)).'</em>';
     echo '<table border="0"><tr><th>Title</th><th>Description</th><th>Depth</th><th>Rank</th><th>Expand</th><th colspan="4">Action</th></tr>'."\n";
     $even=0;
     add_childs(0,0);
     echo '</table><br/>';
-    echo '<a href="menu_edit.php?category=',$category.'&amp;lng='.$lng.'&amp;parent=0">Add root item</a>';
+    echo '<a href="menu_edit.php?category=',$category.'&amp;lng='.$lng.'">Add root item</a>';
 }
 
 require_once('./admin_footer.php');
---
+++ admin/menu_edit.php
@@ -0,0 +1,185 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// / wessie - web site system                                             |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2001 Michal Cihar                                      |
+// +----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to the Free Software          |
+// | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 |
+// | USA                                                                  |
+// +----------------------------------------------------------------------+
+// | Authors: Michal Cihar <cihar at email dot cz>                        |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+$page_name='Menu:Edit';
+require_once('./admin_header.php');
+?>
+<?php
+
+if (isset($id)&&isset($lng)&&!isset($name)){
+    if (!($id_result=(mysql_query('SELECT id,name,description,lng,page,category,parent,expand,rank from '.$table_prepend_name.$table_menu.' where lng='.$lng.' and id='.$id,$db_connection)))&&($child_id=0))
+        do_error(1,'SELECT '.$table_prepend_name.$table_menu.': '.mysql_error());
+    $item = mysql_fetch_array ($id_result);
+    mysql_free_result($id_result);
+
+    if ($item['parent']!=0){
+        if (!($id_result=(mysql_query('SELECT category from '.$table_prepend_name.$table_menu.' where lng='.$lng.' and id='.$item['parent'],$db_connection)))&&($child_id=0))
+            do_error(1,'SELECT '.$table_prepend_name.$table_menu.': '.mysql_error());
+        $parent = mysql_fetch_array ($id_result);
+        mysql_free_result($id_result);
+    }
+
+    if (!($id_result=mysql_query('SELECT name,description,category from '.$table_prepend_name.$table_page.' where lng='.$lng.' and id='.$item['page'].' limit 1',$db_connection)))
+        do_error(1,'SELECT '.$table_prepend_name.$table_page.': '.mysql_error());
+    $page=mysql_fetch_array($id_result);
+    mysql_free_result($id_result);
+} elseif (isset($category)&&isset($lng)&&!isset($name)){
+    $item=array('lng'=>$lng,'category'=>$category,'parent'=>0,'rank'=>0,'expand'=>0);
+    $page=array('name'=>'','description'=>'','category'=>-1);
+
+} elseif (isset($parent)&&isset($lng)&&!isset($name)){
+    if (!($id_result=(mysql_query('SELECT id,category from '.$table_prepend_name.$table_menu.' where lng='.$lng.' and id='.$parent,$db_connection)))&&($child_id=0))
+        do_error(1,'SELECT '.$table_prepend_name.$table_menu.': '.mysql_error());
+    $parent = mysql_fetch_array ($id_result);
+    mysql_free_result($id_result);
+
+    $item=array('lng'=>$lng,'category'=>$parent['category'],'parent'=>$parent['id'],'rank'=>0,'expand'=>0);
+    $page=array('name'=>'','description'=>'','category'=>-1);
+} elseif (isset($id)){
+echo 'save....';
+    if (!($id_result=(mysql_query('UPDATE '.$table_prepend_name.$table_menu.
+            ' set name="'.$name.'"'.
+            ', description="'.$description.'"'.
+            ', page='.$page.
+            ', category='.$category.
+            ', parent='.$parent.
+            ', expand='.$expand.
+            ', lng='.$lng.
+            ', rank='.$rank.
+            ' WHERE id='.$id
+            ,$db_connection)))&&($child_id=0))
+        do_error(1,'UPDATE '.$table_prepend_name.$table_menu.': '.mysql_error());
+
+    if (!($id_result=(mysql_query('SELECT id,name,description,lng,page,category,parent,expand,rank from '.$table_prepend_name.$table_menu.' where lng='.$lng.' and id='.$id,$db_connection)))&&($child_id=0))
+        do_error(1,'SELECT '.$table_prepend_name.$table_menu.': '.mysql_error());
+    $item = mysql_fetch_array ($id_result);
+    mysql_free_result($id_result);
+
+    if ($item['parent']!=0){
+        if (!($id_result=(mysql_query('SELECT category from '.$table_prepend_name.$table_menu.' where lng='.$lng.' and id='.$item['parent'],$db_connection)))&&($child_id=0))
+            do_error(1,'SELECT '.$table_prepend_name.$table_menu.': '.mysql_error());
+        $parent = mysql_fetch_array ($id_result);
+        mysql_free_result($id_result);
+    }
+
+    if (!($id_result=mysql_query('SELECT name,description,category from '.$table_prepend_name.$table_page.' where lng='.$lng.' and id='.$item['page'].' limit 1',$db_connection)))
+        do_error(1,'SELECT '.$table_prepend_name.$table_page.': '.mysql_error());
+    $page=mysql_fetch_array($id_result);
+    mysql_free_result($id_result);
+} else {
+echo 'create new item....';
+    if (!($id_result=(mysql_query('INSERT '.$table_prepend_name.$table_menu.
+            ' set name="'.$name.'"'.
+            ', description="'.$description.'"'.
+            ', page='.$page.
+            ', category='.$category.
+            ', parent='.$parent.
+            ', expand='.$expand.
+            ', lng='.$lng.
+            ', rank='.$rank
+            ,$db_connection)))&&($child_id=0))
+        do_error(1,'INSERT '.$table_prepend_name.$table_menu.': '.mysql_error());
+
+    if (!($id_result=(mysql_query('SELECT id,name,description,lng,page,category,parent,expand,rank from '.$table_prepend_name.$table_menu.' where '.
+            'name="'.$name.'"'.
+            ' and description="'.$description.'"'.
+            ' and page='.$page.
+            ' and category='.$category.
+            ' and parent='.$parent.
+            ' and expand='.$expand.
+            ' and lng='.$lng.
+            ' and rank='.$rank
+            )))&&($child_id=0))
+        do_error(1,'SELECT '.$table_prepend_name.$table_menu.': '.mysql_error());
+    $item = mysql_fetch_array ($id_result);
+    mysql_free_result($id_result);
+
+    if ($item['parent']!=0){
+        if (!($id_result=(mysql_query('SELECT category from '.$table_prepend_name.$table_menu.' where lng='.$lng.' and id='.$item['parent'],$db_connection)))&&($child_id=0))
+            do_error(1,'SELECT '.$table_prepend_name.$table_menu.': '.mysql_error());
+        $parent = mysql_fetch_array ($id_result);
+        mysql_free_result($id_result);
+    }
+
+    if (!($id_result=mysql_query('SELECT name,description,category from '.$table_prepend_name.$table_page.' where lng='.$lng.' and id='.$item['page'].' limit 1',$db_connection)))
+        do_error(1,'SELECT '.$table_prepend_name.$table_page.': '.mysql_error());
+    $page=mysql_fetch_array($id_result);
+    mysql_free_result($id_result);
+}
+?>
+<form action="menu_edit.php" method="post">
+<?php if (isset($id)) echo '<input type="hidden" name="id" value="' . $id . '" />'; ?>
+<input type="hidden" name="lng" value="<?php echo $lng; ?>" />
+<input type="hidden" name="parent" value="<?php echo $item['parent']; ?>" />
+<table border="0">
+<tr><th valign="top"></th><th>Menu item values</th><th>Page <?php echo isset($item['page'])?$item['page']:''; ?> values</th></tr>
+<tr><th valign="top">Name</th><td><?php sized_edit('name',isset($item['name'])?$item['name']:''); ?></td><td><?php echo htmlspecialchars($page['name']); ?></td></tr>
+<tr><th valign="top">Description</th><td><?php sized_textarea('description',isset($item['description'])?$item['description']:'') ?></td><td><?php echo htmlspecialchars($page['description']); ?></td></tr>
+<tr><th valign="top">Page</th><td><?php page_edit(isset($item['page'])?$item['page']:-1,$item['lng'],'page',TRUE); ?></td><td></td></tr>
+<tr><th valign="top">Category</th><td>
+<?php
+if ($item['parent']!=0){
+    echo '<input type="hidden" name="category" value="' . $parent['category'] . '" />' . htmlspecialchars(get_category_name($parent['category'],$item['lng']));
+}else{
+    category_edit($item['category'],$item['lng'],'category');
+}
+?></td><td><?php echo htmlspecialchars(get_category_name($page['category'],$item['lng'])); ?></td></tr>
+<tr><th valign="top">
+Language:
+</th><td>
+<input type="hidden" name="lng" value="<?php echo $item['lng']?>" />
+<?php echo $lang_name[$item['lng']]?>
+</td></tr>
+<tr><th valign="top">Rank</th><td><input type="text" name="rank" value="<?php echo $item['rank']; ?>" /></td><td></td></tr>
+<tr><th valign="top">Expand</th><td><select name="expand">
+<?php
+if ($item['expand']==0)
+    echo'<option value="0" selected="selected">';
+else
+    echo'<option value="0">';
+?>When selected</option>
+<?php
+if ($item['expand']==1)
+    echo'<option value="1" selected="selected">';
+else
+    echo'<option value="1">';
+?>Always</option>
+</select>
+</td><td></td></tr>
+<tr><th valign="top">
+</th><td>
+<table border="0" width="100%">
+  <tr>
+    <td align="center"><input type="submit" value=" Save " /></td>
+    <td align="center"><input type="reset" value=" Reset " /></td>
+  </tr>
+</table>
+</td><td></td></tr>
+</table>
+</form>
+<?php
+require_once('./admin_footer.php');
+?>
