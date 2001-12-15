9bb2fcc9e67d3d3fa543f545f7b81948044fb2e2
Michal Čihař <michal@cihar.com>
2001-12-15 19:09:39 +0000

    auth is now enabled

--- admin/auth.php
+++ admin/auth.php
@@ -39,28 +39,28 @@ if (isset($REQUEST_URI)){
     $url = '';
 }
 
-/*if((!isset($HTTP_COOKIE_VARS['user']))||(!isset($HTTP_COOKIE_VARS['hash']))){
+if((!isset($HTTP_COOKIE_VARS['user']))||(!isset($HTTP_COOKIE_VARS['hash']))){
 header('Location: http://'.$SERVER_NAME.dirname($REQUEST_URI).(substr(dirname($REQUEST_URI),-5)!='admin'?'admin':'').'/login.php?failure=unauthorised' . $url);
 die();
 }
 $user=$HTTP_COOKIE_VARS['user'];
-$hash=$HTTP_COOKIE_VARS['hash'];*/
+$hash=$HTTP_COOKIE_VARS['hash'];
 $user='admin';
 
 if (!($id_result=mysql_query('SELECT count(user) as count from '.$table_prepend_name.$table_users.' where user="'.$user.'" and hash="'.$hash.'"',$db_connection)))
     do_error(1,'SELECT '.$table_prepend_name.$table_users.': '.mysql_error());
 $auth=mysql_fetch_array($id_result);
 mysql_free_result($id_result);
-/*if ($auth['count']!=1){
+if ($auth['count']!=1){
     Header('Location: http://'.$SERVER_NAME.dirname($REQUEST_URI).'/login.php?failure=unauthorised' . $url);
     die();
-}*/
+}
 if (!($id_result=mysql_query('SELECT ip from '.$table_prepend_name.$table_users.' where user="'.$user.'" and hash="'.$hash.'" and time>(NOW() - interval '.$admin_timeout.')',$db_connection)))
     do_error(1,'SELECT '.$table_prepend_name.$table_users.': '.mysql_error());
-/*if (mysql_num_rows($id_result)!=1){
+if (mysql_num_rows($id_result)!=1){
     Header('Location: http://'.$SERVER_NAME.dirname($REQUEST_URI).'/login.php?failure=expired' . $url);
     die();
-}*/
+}
 
 $ip=$REMOTE_ADDR;
 $headers = getallheaders();
@@ -72,13 +72,13 @@ while (list ($header, $value) = each ($headers)) {
 
 $auth=mysql_fetch_array($id_result);
 mysql_free_result($id_result);
-/*if ($auth['ip']!=$ip){
+if ($auth['ip']!=$ip){
     Header('Location: http://'.$SERVER_NAME.dirname($REQUEST_URI).'/login.php?failure=badip' . $url);
     die();
-}*/
+}
 
-/*if (!(mysql_query('UPDATE '.$table_prepend_name.$table_users." set time=NOW() where user='".$user."' and hash='".$hash."' limit 1",$db_connection)))
-    do_error(1,'UPDATE '.$table_prepend_name.$table_users.': '.mysql_error());*/
+if (!(mysql_query('UPDATE '.$table_prepend_name.$table_users." set time=NOW() where user='".$user."' and hash='".$hash."' limit 1",$db_connection)))
+    do_error(1,'UPDATE '.$table_prepend_name.$table_users.': '.mysql_error());
 
 setcookie ("hash",$hash ,time()+3600, dirname($REQUEST_URI));//valid one hour
 ?>
