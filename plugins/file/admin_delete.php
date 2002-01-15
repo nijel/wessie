015d8fce4a93820eed4a0ab2ece89011524f31d1
Michal Čihař <michal@cihar.com>
2002-01-15 13:12:54 +0000

    file plugin support deleting

--- plugins/file/admin_delete.php
+++ plugins/file/admin_delete.php
@@ -25,39 +25,35 @@
 //
 // $Id$
 
-function delete_article(){
-    global $id,$lng,$table_prepend_name,$table_article,$table_page,$form_done_url;
+function delete_page(){
+    global $id,$lng,$table_prepend_name,$table_page,$form_done_url;
 
     if (!mysql_query('DELETE FROM '.$table_prepend_name.$table_page.' where id='.$id.' and lng='.$lng.' limit 1')){
         show_error("Can't delete page! (".mysql_error().')');
         exit;
     }
-    if (!mysql_query('DELETE FROM '.$table_prepend_name.$table_article.' where page='.$id.' and lng='.$lng.' limit 1')){
-        show_error("Can't delete page! (".mysql_error().')');
-        exit;
-    }
-    show_info_box('Article deleted',array(),$form_done_url);
+    show_info_box('Page deleted',array(),$form_done_url);
     include_once('./admin_footer.php');
     exit;
 }
 
 if (isset($action) && ($action=='delete') && isset($lng) && isset($id)){
-    delete_article();
+    delete_page();
 }elseif (isset($lng) && isset($id)){
     if ($admin_confirm_delete){
         if (!$id_result=mysql_query(
-        'SELECT UNIX_TIMESTAMP(last_change) as last_change, page, '.$table_prepend_name.$table_article.'.lng as lng, name, description, keywords, count, category, content '.
-        ' from '.$table_prepend_name.$table_article.','.$table_prepend_name.$table_page.
-        ' where id=page and '.$table_prepend_name.$table_article.'.lng='.$table_prepend_name.$table_page.'.lng and '.$table_prepend_name.$table_article.'.lng='.$lng.' and id='.$id))
-            show_error("Can't get article info! (".mysql_error().')');
-        $article=mysql_fetch_array($id_result);
+            'SELECT lng, id, name, description, count, category, param, keywords'.
+            ' from '.$table_prepend_name.$table_page.
+            ' where lng='.$lng.' and id='.$id))
+            show_error("Can't get page info! (".mysql_error().')');
+        $page=mysql_fetch_array($id_result);
         mysql_free_result($id_result);
-        if (!isset($article['page'])){
+        if (!isset($page['id'])){
             show_error_box("This page doesn't  exist!");
             exit();
         }
     }else{
-        delete_article();
+        delete_page();
     }
 }else{
     show_error_box();
@@ -65,14 +61,21 @@ if (isset($action) && ($action=='delete') && isset($lng) && isset($id)){
     exit();
 }
 
+$filename='UNKNOWN';
+$pre=TRUE;
+$code=TRUE;
+$html=TRUE;
+$highlight=FALSE;
+eval($page['param']);
+
 ?>
-Do you want to delete following article?<br />
+Do you want to delete following page?<br />
 <table class="yesno">
   <tr>
     <td>
 <form action="<?php echo $form_action; ?>" method="post" class="delete">
-<input type="hidden" name="id" value="<?php echo $article['page']; ?>" />
-<input type="hidden" name="lng" value="<?php echo $article['lng']; ?>" />
+<input type="hidden" name="id" value="<?php echo $page['id']; ?>" />
+<input type="hidden" name="lng" value="<?php echo $page['lng']; ?>" />
 <input type="hidden" name="action" value="delete" />
 <input type="submit" value=" Yes " class="delete" />
 <?php echo $form_magic;?>
@@ -81,51 +84,67 @@ Do you want to delete following article?<br />
     <td><?php make_return_button(' No ');?> </td>
   </tr>
 </table>
-<a href="<?php echo make_url($id,$lng)?>" target="_blank">Here</a> you can view article rendered in template.<br />
+<a href="<?php echo make_url($id,$lng)?>" target="_blank">Here</a> you can view page rendered in template.<br />
 
 <table class="item">
 <tr><th>
 Page ID:
 </th><td>
-<?php echo ($article['page']!=-1?$article['page']:'Page not created yet')?>
+<?php echo ($page['id']!=-1?$page['id']:'Page not created yet')?>
 </td></tr>
 <tr><th>
 Language:
 </th><td>
-<?php echo $lang_name[$article['lng']]?>
+<?php echo $lang_name[$page['lng']]?>
 </td></tr>
 <tr><th>
-Last change:
+Name:
 </th><td>
-<?php
-    echo strftime('%c',$article['last_change']);
-?>
-</td></tr>
-<tr><th>
-Title:
-</th><td>
-<?php echo htmlspecialchars($article['name']) ?>
+<?php echo htmlspecialchars($page['name']) ?>
 </td></tr>
 <tr><th>
 Category:
 </th><td>
-<?php echo htmlspecialchars(get_category_name($article['category'],$article['lng'])) ?>
+<?php echo htmlspecialchars(get_category_name($page['category'],$page['lng'])) ?>
 </td></tr>
 
 
 <tr><th>
 Description:
 </th><td>
-<?php echo htmlspecialchars($article['description']) ?>
+<?php echo htmlspecialchars($page['description']) ?>
 </td></tr>
 <tr><th>
 Keywords:
 </th><td>
-<?php echo htmlspecialchars($article['keywords']) ?>
+<?php echo htmlspecialchars($page['keywords']) ?>
+</td></tr>
+
+<tr><th>
+Filename:
+</th><td>
+<?php echo htmlspecialchars($filename) ?>
+</td></tr>
+
+<tr><th>
+Highlight php:
+</th><td>
+<?php if ($highlight) echo 'Yes'; else echo 'No'; ?>
 </td></tr>
 <tr><th>
-Content:
+Add <code>&lt;pre&gt;</code>:
 </th><td>
-<?php echo htmlspecialchars($article['content']) ?>
+<?php if ($pre) echo 'Yes'; else echo 'No'; ?>
 </td></tr>
+<tr><th>
+Add <code>&lt;code&gt;</code>:
+</th><td>
+<?php if ($code) echo 'Yes'; else echo 'No'; ?>
+</td></tr>
+<tr><th>
+Replace html entities:
+</th><td>
+<?php if ($html) echo 'Yes'; else echo 'No'; ?>
+</td></tr>
+
 </table>
