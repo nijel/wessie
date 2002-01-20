539dd070ee49866472c02d4d201295a0fc26c633
Michal Čihař <michal@cihar.com>
2002-01-20 19:58:46 +0000

    updated sql dump

--- sql/site.sql
+++ sql/site.sql
@@ -4,7 +4,7 @@
 # http://phpmyadmin.sourceforge.net/ (download page)
 #
 # Host: localhost
-# Generation Time: Jan 13, 2002 at 03:23 PM
+# Generation Time: Jan 20, 2002 at 08:58 PM
 # Server version: 3.23.47
 # PHP Version: 4.1.1
 # Database : `wessie`
@@ -46,7 +46,7 @@ CREATE TABLE article (
 # Dumping data for table `article`
 #
 
-INSERT INTO article VALUES ('<h1>Welcome to wessie</h1>\r\n\r\n<h2>wessie was successfully installed and is working</h2>\r\n\r\nWeb Site System was installed on this site, but no content wasn\'t\r\n(probably) added yet.<br />\r\nwessie home page can be found on <a href=\r\n"http://cicom.tsx.org"><b>cicom.tsx.org</b></a>.\r\n\r\n<h2>VERY quick guide how to manage wessie</h2>\r\n\r\n<p>If you are looking for administration check <a href=\r\n"<?php echo $base_path ; ?>admin">this</a> link. Default administrator can login with name\r\n<code>admin</code> and password <code>admin</code>. <b>It is highly\r\nrecommended to change password to not so easilly guessable password\r\n(<code>admin:admin</code> is really NOT a good choice for\r\nfuture).</b></p>\r\n\r\n<p>Here you can download readme file:<br />\r\n<?php download(1); ?>\r\n</p>\r\n\r\n<p>Here you can download beta distribution:<br />\r\n<?php download(2); ?>\r\n</p>\r\n\r\n<p>And here you can download cicom site:<br />\r\n<?php download(3); ?>\r\n</p>\r\n\r\n<p>And here you can download CodeConv 2.6 with Updater 1.1:<br />\r\n<?php download(4); ?>\r\n</p>', 20011210124958, 1, 0);
+INSERT INTO article VALUES ('<h1>Welcome to wessie</h1>\r\n\r\n<h2>wessie was successfully installed and is working</h2>\r\n\r\nWeb Site System was installed on this site, but no content wasn\'t\r\n(probably) added yet.<br />\r\nwessie home page can be found on <a href=\r\n"http://cicom.tsx.org"><b>cicom.tsx.org</b></a>.\r\n\r\n<h2>VERY quick guide how to manage wessie</h2>\r\n\r\n<p>If you are looking for administration check <a href=\r\n"<?php echo $base_path ; ?>admin">this</a> link. Default administrator can login with name\r\n<code>admin</code> and password <code>admin</code>. <b>It is highly\r\nrecommended to change password to not so easilly guessable password\r\n(<code>admin:admin</code> is really NOT a good choice for\r\nfuture).</b></p>\r\n\r\n<p>Here you can download readme file:<br />\r\n<?php download(1); ?>\r\n</p>\r\n\r\n<p>Here you can download beta distribution:<br />\r\n<?php download(2); ?>\r\n</p>\r\n\r\n<p>And here you can download cicom site:<br />\r\n<?php download(3); ?>\r\n</p>\r\n\r\n<p>And here you can download CodeConv 2.6 with Updater 1.1:<br />\r\n<?php download(4); ?>\r\n</p>', 20020113174349, 1, 0);
 INSERT INTO article VALUES ('Information about wessie:<br />\r\n<table>\r\n<tr>\r\n<th>wessie version</th>\r\n<td><?php echo $wessie_version; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>wessie author</th>\r\n<td><?php echo $wessie_author; ?>\r\n&lt;<?php echo $wessie_author_email; ?>\r\n&gt;</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>wessie home page</th>\r\n<td><?php echo $wessie_url; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>wessie icon:</th>\r\n<td><?php wessie_icon(); ?>\r\n</td>\r\n</tr>\r\n\r\n</table>\r\n\r\n<br />\r\nInformation about this server:<br />\r\n<table>\r\n<tr>\r\n<th>Software</th>\r\n<td><?php echo $SERVER_SOFTWARE; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>Signature</th>\r\n<td><?php echo $SERVER_SIGNATURE; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>Protocol</th>\r\n<td><?php echo $SERVER_PROTOCOL; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>Name</th>\r\n<td><?php echo $SERVER_NAME; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>Address</th>\r\n<td><?php echo $SERVER_ADDR; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>Port</th>\r\n<td><?php echo $SERVER_PORT; ?>\r\n</td>\r\n</tr>\r\n</table>\r\n\r\n<br />\r\nInformation about this connection:<br />\r\n<table>\r\n<tr>\r\n<th>User agent</th>\r\n<td><?php echo $HTTP_USER_AGENT; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>User browser</th>\r\n<td><?php echo $browser; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>User operating system</th>\r\n<td><?php echo $os; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>Request URI</th>\r\n<td><?php echo $REQUEST_URI; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>Remote address</th>\r\n<td><?php echo $REMOTE_ADDR; ?>\r\n</td>\r\n</tr>\r\n\r\n<tr>\r\n<th>HTTP referer</th>\r\n<td><?php echo $HTTP_REFERER; ?>\r\n</td>\r\n</tr>\r\n</table>\r\n\r\n<br />\r\nCurrent server time is <?php echo strftime(\'%x\'); ?>\r\n, <?php echo strftime(\'%X\'); ?>\r\n<br />\r\n<br />', 20011016232611, 2, 0);
 INSERT INTO article VALUES ('Information about <?php echo $site_name[$lng]; ?>:<br />\r\nThis site was created by <?php echo $site_author[$lng]; ?> &lt;<?php echo $site_author_email; ?>&gt;. Name of this site is <?php echo $site_name[$lng]; ?>\r\n and it\'s base url is <?php echo $site_home; ?>. Title of this page is <?php page_title(); ?>  and it is in category called <?php category_name(); ?>. This site was built using <?php echo $wessie_version; ?>.', 20011215203422, 3, 0);
 INSERT INTO article VALUES ('Here will be some short help...', 20010508224252, 4, 0);
@@ -259,7 +259,7 @@ DROP TABLE IF EXISTS page;
 CREATE TABLE page (
   id smallint(5) unsigned NOT NULL auto_increment,
   name varchar(100) NOT NULL default '',
-  type enum('article','file','links','bullshit') default NULL,
+  type enum('article','links','bullshit','file') default NULL,
   param text NOT NULL,
   description text NOT NULL,
   keywords text NOT NULL,
@@ -274,25 +274,26 @@ CREATE TABLE page (
 # Dumping data for table `page`
 #
 
-INSERT INTO page VALUES (1, 'Welcome page', 'article', '', 'Welcome page', 'wessie', 14536, 0, 1);
-INSERT INTO page VALUES (2, 'About', 'article', '', 'Information about Web Site System', 'wessie', 96, 0, 2);
-INSERT INTO page VALUES (3, '<?php echo $site_name[$lng]; ?>', 'article', '', 'Information about <?php echo $site_name[$lng]; ?>', '<?php echo $site_name[$lng]; ?>', 71, 0, 3);
-INSERT INTO page VALUES (4, 'Help', 'article', '', 'How to use Web Site System', 'wessie,Help', 1247, 0, 4);
-INSERT INTO page VALUES (6, 'Statistics', 'article', '', 'Statistical page', 'wessie,statistics', 1607, 0, 1);
-INSERT INTO page VALUES (7, 'Weekly statistics', 'article', '', 'Statistical page', 'wessie,statistics', 176, 0, 1);
-INSERT INTO page VALUES (8, 'Daily statistics', 'article', '', 'Statistical page', 'wessie,statistics', 126, 0, 1);
-INSERT INTO page VALUES (9, 'Hourly statistics', 'article', '', 'Statistical page', 'wessie,statistics', 165, 0, 1);
-INSERT INTO page VALUES (1, 'Úvodní stránka', 'article', '', 'Úvodní stránka', 'wessie', 370, 1, 1);
-INSERT INTO page VALUES (2, 'O aplikaci', 'article', '', 'Informace o Web Site System', 'wessie', 129, 1, 2);
-INSERT INTO page VALUES (3, '<?php echo $site_name[$lng]; ?>', 'article', '', 'Informace o <?php echo $site_name[$lng]; ?>', '<?php echo $site_name[$lng]; ?>', 119, 1, 3);
-INSERT INTO page VALUES (4, 'Nápovìda', 'article', '', 'Jak pou¾ívat Web Site System', 'wessie,Help', 87, 1, 4);
+INSERT INTO page VALUES (1, 'Welcome page', 'article', '', 'Welcome page', 'wessie', 14543, 0, 1);
+INSERT INTO page VALUES (2, 'About', 'article', '', 'Information about Web Site System', 'wessie', 107, 0, 2);
+INSERT INTO page VALUES (3, '<?php echo $site_name[$lng]; ?>', 'article', '', 'Information about <?php echo $site_name[$lng]; ?>', '<?php echo $site_name[$lng]; ?>', 78, 0, 3);
+INSERT INTO page VALUES (4, 'Help', 'article', '', 'How to use Web Site System', 'wessie,Help', 1250, 0, 4);
+INSERT INTO page VALUES (6, 'Statistics', 'article', '', 'Statistical page', 'wessie,statistics', 1610, 0, 1);
+INSERT INTO page VALUES (7, 'Weekly statistics', 'article', '', 'Statistical page', 'wessie,statistics', 177, 0, 1);
+INSERT INTO page VALUES (8, 'Daily statistics', 'article', '', 'Statistical page', 'wessie,statistics', 129, 0, 1);
+INSERT INTO page VALUES (9, 'Hourly statistics', 'article', '', 'Statistical page', 'wessie,statistics', 167, 0, 1);
+INSERT INTO page VALUES (1, 'Úvodní stránka', 'article', '', 'Úvodní stránka', 'wessie', 372, 1, 1);
+INSERT INTO page VALUES (2, 'O aplikaci', 'article', '', 'Informace o Web Site System', 'wessie', 131, 1, 2);
+INSERT INTO page VALUES (3, '<?php echo $site_name[$lng]; ?>', 'article', '', 'Informace o <?php echo $site_name[$lng]; ?>', '<?php echo $site_name[$lng]; ?>', 123, 1, 3);
+INSERT INTO page VALUES (4, 'Nápovìda', 'article', '', 'Jak pou¾ívat Web Site System', 'wessie,Help', 88, 1, 4);
 INSERT INTO page VALUES (6, 'Statistiky', 'article', '', 'Stránka statistik', 'wessie,statistics', 353, 1, 1);
 INSERT INTO page VALUES (7, 'Týdení statistiky', 'article', '', 'Stránka statistik', 'wessie,statistics', 137, 1, 1);
-INSERT INTO page VALUES (8, 'Denní statistiky', 'article', '', 'Stránka statistik', 'wessie,statistics', 123, 1, 1);
-INSERT INTO page VALUES (9, 'Hodinové statistiky', 'article', '', 'Stránka statistik', 'wessie,statistics', 130, 1, 1);
-INSERT INTO page VALUES (11, '/etc/passwd', 'file', '/etc/passwd', '/etc/passwd listing', 'etc passwd', 17, 1, 1);
-INSERT INTO page VALUES (12, 'test', 'article', '', 'test', 'test', 1, 0, 1);
-INSERT INTO page VALUES (12, 'test', 'bullshit', '$pars=10;', 'test', 'test', 5, 1, 1);
+INSERT INTO page VALUES (8, 'Denní statistiky', 'article', '', 'Stránka statistik', 'wessie,statistics', 124, 1, 1);
+INSERT INTO page VALUES (9, 'Hodinové statistiky', 'article', '', 'Stránka statistik', 'wessie,statistics', 131, 1, 1);
+INSERT INTO page VALUES (11, '/etc/passwd', 'file', '$filename=\'/etc/passwd\';\r\n$pre=TRUE;', '/etc/passwd listing', 'etc passwd', 24, 1, 1);
+INSERT INTO page VALUES (12, 'test', 'article', '', 'test', 'test', 7, 0, 1);
+INSERT INTO page VALUES (12, 'test', 'bullshit', '$pars=5;\n$sentences=50;\n$words=20;\n$letters=10;\n$addHtml=3;', 'test', 'test', 11, 1, 1);
+INSERT INTO page VALUES (11, 'db connect', 'file', '$filename=\'/vs/wessie/admin/admin_header.php\';\n$pre=TRUE;\n$code=TRUE;\n$html=TRUE;\n$highlight=TRUE;\n', 'db connect', 'mysql', 7, 0, 1);
 # --------------------------------------------------------
 
 #
@@ -311,7 +312,7 @@ CREATE TABLE stat (
 # Dumping data for table `stat`
 #
 
-INSERT INTO stat VALUES ('total', 'hits', 19108);
+INSERT INTO stat VALUES ('total', 'hits', 19186);
 INSERT INTO stat VALUES ('time', '00', 95);
 INSERT INTO stat VALUES ('time', '01', 37);
 INSERT INTO stat VALUES ('time', '02', 1);
@@ -322,34 +323,34 @@ INSERT INTO stat VALUES ('time', '06', 0);
 INSERT INTO stat VALUES ('time', '07', 0);
 INSERT INTO stat VALUES ('time', '08', 1);
 INSERT INTO stat VALUES ('time', '09', 9);
-INSERT INTO stat VALUES ('time', '10', 14);
-INSERT INTO stat VALUES ('time', '11', 493);
-INSERT INTO stat VALUES ('time', '12', 205);
-INSERT INTO stat VALUES ('time', '13', 431);
-INSERT INTO stat VALUES ('time', '14', 1568);
-INSERT INTO stat VALUES ('time', '15', 2295);
-INSERT INTO stat VALUES ('time', '16', 40);
-INSERT INTO stat VALUES ('time', '17', 87);
-INSERT INTO stat VALUES ('time', '18', 221);
-INSERT INTO stat VALUES ('time', '19', 62);
-INSERT INTO stat VALUES ('time', '20', 168);
-INSERT INTO stat VALUES ('time', '21', 1244);
-INSERT INTO stat VALUES ('time', '22', 368);
+INSERT INTO stat VALUES ('time', '10', 15);
+INSERT INTO stat VALUES ('time', '11', 500);
+INSERT INTO stat VALUES ('time', '12', 207);
+INSERT INTO stat VALUES ('time', '13', 434);
+INSERT INTO stat VALUES ('time', '14', 1572);
+INSERT INTO stat VALUES ('time', '15', 2297);
+INSERT INTO stat VALUES ('time', '16', 45);
+INSERT INTO stat VALUES ('time', '17', 92);
+INSERT INTO stat VALUES ('time', '18', 230);
+INSERT INTO stat VALUES ('time', '19', 63);
+INSERT INTO stat VALUES ('time', '20', 192);
+INSERT INTO stat VALUES ('time', '21', 1258);
+INSERT INTO stat VALUES ('time', '22', 369);
 INSERT INTO stat VALUES ('time', '23', 9703);
-INSERT INTO stat VALUES ('dow', '1', 3717);
-INSERT INTO stat VALUES ('dow', '2', 1348);
-INSERT INTO stat VALUES ('dow', '3', 8418);
+INSERT INTO stat VALUES ('dow', '1', 3718);
+INSERT INTO stat VALUES ('dow', '2', 1359);
+INSERT INTO stat VALUES ('dow', '3', 8422);
 INSERT INTO stat VALUES ('dow', '4', 2975);
-INSERT INTO stat VALUES ('dow', '5', 1998);
-INSERT INTO stat VALUES ('dow', '6', 70);
-INSERT INTO stat VALUES ('dow', '7', 609);
+INSERT INTO stat VALUES ('dow', '5', 2025);
+INSERT INTO stat VALUES ('dow', '6', 75);
+INSERT INTO stat VALUES ('dow', '7', 639);
 INSERT INTO stat VALUES ('browser', 'Links', 9);
 INSERT INTO stat VALUES ('browser', 'WebTV', 0);
 INSERT INTO stat VALUES ('browser', 'Lynx', 4);
 INSERT INTO stat VALUES ('browser', 'MSIE', 16);
 INSERT INTO stat VALUES ('browser', 'Opera', 23);
-INSERT INTO stat VALUES ('browser', 'Konqueror', 2179);
-INSERT INTO stat VALUES ('browser', 'Mozilla', 286);
+INSERT INTO stat VALUES ('browser', 'Konqueror', 2182);
+INSERT INTO stat VALUES ('browser', 'Mozilla', 346);
 INSERT INTO stat VALUES ('browser', 'Netscape', 7);
 INSERT INTO stat VALUES ('browser', 'NCSA Mosaic', 0);
 INSERT INTO stat VALUES ('browser', 'W3C_Validator', 0);
@@ -363,11 +364,11 @@ INSERT INTO stat VALUES ('browser', 'Amaya', 0);
 INSERT INTO stat VALUES ('browser', 'Downloader', 0);
 INSERT INTO stat VALUES ('browser', 'Search Indexer', 0);
 INSERT INTO stat VALUES ('browser', 'Bot', 4639);
-INSERT INTO stat VALUES ('browser', '?', 11950);
+INSERT INTO stat VALUES ('browser', '?', 11965);
 INSERT INTO stat VALUES ('os', 'Windows', 2);
 INSERT INTO stat VALUES ('os', 'WindowsNT', 3);
 INSERT INTO stat VALUES ('os', 'Amiga', 0);
-INSERT INTO stat VALUES ('os', 'Linux/Unix', 7089);
+INSERT INTO stat VALUES ('os', 'Linux/Unix', 7152);
 INSERT INTO stat VALUES ('os', 'Mac', 2);
 INSERT INTO stat VALUES ('os', 'FreeBSD', 0);
 INSERT INTO stat VALUES ('os', 'SunOS', 0);
@@ -375,10 +376,10 @@ INSERT INTO stat VALUES ('os', 'IRIX', 0);
 INSERT INTO stat VALUES ('os', 'BeOS', 0);
 INSERT INTO stat VALUES ('os', 'OS/2', 0);
 INSERT INTO stat VALUES ('os', 'AIX', 0);
-INSERT INTO stat VALUES ('os', '?', 12012);
+INSERT INTO stat VALUES ('os', '?', 12027);
 INSERT INTO stat VALUES ('week_no', '0016', 1696);
-INSERT INTO stat VALUES ('lang', '0', 17750);
-INSERT INTO stat VALUES ('lang', '1', 1315);
+INSERT INTO stat VALUES ('lang', '0', 17804);
+INSERT INTO stat VALUES ('lang', '1', 1339);
 INSERT INTO stat VALUES ('week_no', '0017', 58);
 INSERT INTO stat VALUES ('week_no', '0018', 37);
 INSERT INTO stat VALUES ('week_no', '0019', 30);
@@ -405,7 +406,8 @@ INSERT INTO stat VALUES ('week_no', '0048', 504);
 INSERT INTO stat VALUES ('week_no', '0049', 5);
 INSERT INTO stat VALUES ('week_no', '0050', 2);
 INSERT INTO stat VALUES ('week_no', '0051', 66);
-INSERT INTO stat VALUES ('week_no', '0052', 32);
+INSERT INTO stat VALUES ('week_no', '0052', 85);
+INSERT INTO stat VALUES ('week_no', '0053', 26);
 # --------------------------------------------------------
 
 #
@@ -414,25 +416,23 @@ INSERT INTO stat VALUES ('week_no', '0052', 32);
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id smallint(5) unsigned NOT NULL auto_increment,
   user varchar(20) NOT NULL default '',
   pass varchar(32) NOT NULL default '',
   name varchar(50) NOT NULL default '',
   mail varchar(50) NOT NULL default '',
   web varchar(100) NOT NULL default '',
-  web_title varchar(100) NOT NULL default '',
-  rights text NOT NULL,
+  perms text NOT NULL,
   ip varchar(33) NOT NULL default '',
   hash varchar(32) NOT NULL default '',
   time timestamp(14) NOT NULL,
-  PRIMARY KEY  (id),
-  UNIQUE KEY user (user),
-  KEY user_2 (user)
+  PRIMARY KEY  (user)
 ) TYPE=MyISAM;
 
 #
 # Dumping data for table `users`
 #
 
-INSERT INTO users VALUES (1, 'admin', 'admin', 'Administrator', '', 'http://cicom.kgb.cz', 'CICOM web site - home of wessie', '', '192.168.1.1', '3fe9d6e970a772302f8feaebf5f539cd', 20020113151144);
+INSERT INTO users VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3', 'Administrator', '', 'http://cicom.kgb.cz', '', '192.168.1.1', 'a3f711e4375ac8ce7ccc25b4ff9a6060', 20020120204536);
+INSERT INTO users VALUES ('michal', '06b2af75179fb94be097af182a442a4a', 'Michal Cihar', 'cihar@cicom.cic', 'http://www2.cicom.cic', 'category.php:category_delete.php:category_edit.php:download_group.php:download_group_delete.php:download_group_edit.php:download_item.php:download_item_delete.php:download_item_edit.php:file_list.php:files.php:files_action.php:files_upload.php:help.php:index.php:menu.php:menu_delete.php:menu_edit.php:menu_sync.php:options.php:options_admin.php:options_databae.php:options_design.php:options_errors.php:options_info.php:options_languages.php:page.php:page_delete.php:page_edit.php:page_list.php:user.php:user_delete.php:user_edit.php:user_self.php', '192.168.1.1', '1a5eae4dbdec7b12b4ab692de5aa6b6f', 20020120144759);
+INSERT INTO users VALUES ('test', '098f6bcd4621d373cade4e832627b4f6', 'T3S7', 'test@cicom.cic', 'http://test.cic', 'help.php:index.php:user_self.php', '192.168.1.1', 'c914705464adde59d13ba41d18d91e52', 20020120144724);
 
